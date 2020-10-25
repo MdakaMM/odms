cfa6f47de652806e4aae0eed69160f9b2477fb09
MdakaMM <43600557+MdakaMM@users.noreply.github.com>
2020-10-26 00:44:41 +0200

    remote connection

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,10 +1,17 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','odmsdb');
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','odmsdb');
 // Establish database connection.
+
+// DB credentials.
+define('DB_HOST','db4free.net');
+define('DB_USER','mdakamm94');
+define('DB_PASS','Musa@1994');
+define('DB_NAME','m9419mdakamm');
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -12,6 +12,7 @@ define('DB_HOST','db4free.net');
 define('DB_USER','mdakamm94');
 define('DB_PASS','Musa@1994');
 define('DB_NAME','m9419mdakamm');
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
